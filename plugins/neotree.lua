895bcf72ca62e2dee6aef376e3567a7c50fd956d
esterlein <esterlein.git@gmail.com>
2024-11-12 18:49:39 -0500

    init

--- plugins/neotree.lua
+++ plugins/neotree.lua
@@ -1,12 +1,26 @@
 return {
 	{
 		'nvim-neo-tree/neo-tree.nvim',
+		opts = {
+			window = {
+				position = 'left',
+				width = 40,
+			},
+			filesystem = {
+				filtered_items = {
+					visible = true,
+					show_hidden_count = true,
+					hide_dotfiles = false,
+					hide_gitignored = false,
+				},
+			},
+		},
 		branch = 'v3.x',
 		dependencies = {
 			'nvim-lua/plenary.nvim',
 			'nvim-tree/nvim-web-devicons',
 			'MunifTanjim/nui.nvim',
-			-- "3rd/image.nvim", -- image support in preview
+			'3rd/image.nvim',
 		},
 	},
 }
