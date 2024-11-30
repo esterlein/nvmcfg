5b66a162695266ffc7a3b6e783684fbecf4245c1
esterlein <esterlein.git@gmail.com>
2024-11-30 13:47:53 -0500

    semicolon at eol

--- keymap.lua
+++ keymap.lua
@@ -13,3 +13,11 @@ vim.keymap.set('n', '<C-h>', '<C-w><C-h>', { desc = 'Move focus to the left wind
 vim.keymap.set('n', '<C-l>', '<C-w><C-l>', { desc = 'Move focus to the right window' })
 vim.keymap.set('n', '<C-j>', '<C-w><C-j>', { desc = 'Move focus to the lower window' })
 vim.keymap.set('n', '<C-k>', '<C-w><C-k>', { desc = 'Move focus to the upper window' })
+
+-- semicolon at eol
+
+vim.keymap.set('n', '<leader>;', function()
+	local cursor = vim.api.nvim_win_get_cursor(0)
+	vim.cmd 'norm A;'
+	vim.api.nvim_win_set_cursor(0, cursor)
+end, { noremap = true, silent = true })
