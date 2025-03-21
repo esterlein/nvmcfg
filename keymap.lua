3d6ba7740540130bd8fc67d66a486b1ce043e59a
esterlein <esterlein.git@gmail.com>
2025-03-21 13:31:11 -0400

    keymap

--- keymap.lua
+++ keymap.lua
@@ -14,6 +14,8 @@ vim.keymap.set('n', '<C-l>', '<C-w><C-l>', { desc = 'Move focus to the right win
 vim.keymap.set('n', '<C-j>', '<C-w><C-j>', { desc = 'Move focus to the lower window' })
 vim.keymap.set('n', '<C-k>', '<C-w><C-k>', { desc = 'Move focus to the upper window' })
 
+vim.api.nvim_set_keymap('n', '<leader>rn', '<cmd>lua vim.lsp.buf.rename()<CR>', { noremap = true, silent = true })
+
 -- semicolon at eol
 
 vim.keymap.set('n', '<leader>;', function()
