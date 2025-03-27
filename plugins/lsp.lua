56ee3f1ac1d1b1e5dcb330898265a256f73471a4
esterlein <esterlein.git@gmail.com>
2025-03-26 21:59:04 -0400

    fix lsp

--- plugins/lsp.lua
+++ plugins/lsp.lua
@@ -85,7 +85,7 @@ return {
 					cmd = {
 						'clangd',
 						'--query-driver=/usr/local/Cellar/gcc/14.2.0_1/include',
-						'--backgrouhd-index',
+						'--backgroud-index',
 						'--clang-tidy',
 					},
 					filetypes = { 'cpp', 'c', 'h', 'hpp', 'tpp' },
