1af1229b1e4b6dc1a2f2fed3c029f223c98a54c1
esterlein <esterlein.git@gmail.com>
2025-06-24 23:18:51 -0400

    hypr

--- setup.sh
+++ setup.sh
@@ -8,4 +8,5 @@ ln -s ~/nvmcfg/usercmd.lua ~/.config/nvim/lua/usercmd.lua
 ln -s ~/nvmcfg/plugins ~/.config/nvim/lua/plugins
 ln -s ~/nvmcfg/.stylua.toml ~/.config/nvim/.stylua.toml
 ln -s ~/nvmcfg/kitty.conf ~/.config/kitty/kitty.conf
+ln -s ~/nvmcfg/protozerg.conf ~/.config/kitty/protozerg.conf
 ln -s ~/nvmcfg/.clangd ~/.clangd
