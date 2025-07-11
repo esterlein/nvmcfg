e17ae52b5de7152f478423a9f625acf388e399fd
esterlein <esterlein.git@gmail.com>
2025-07-10 21:14:57 -0400

    daplog

--- usercmd.lua
+++ usercmd.lua
@@ -15,3 +15,14 @@ vim.api.nvim_create_user_command('Hr', function(opts)
 	end
 	vim.cmd(':resize ' .. ((vim.opt.lines:get() - vim.opt.cmdheight:get()) * (opts.args / 100.0)))
 end, { nargs = '*' })
+
+vim.api.nvim_create_user_command('DapLog', function()
+	local log_path = vim.fn.stdpath 'cache' .. '/dap.log'
+	if vim.fn.filereadable(log_path) == 0 then
+		vim.notify('DAP log not found at: ' .. log_path, vim.log.levels.WARN)
+		return
+	end
+	vim.cmd('vsplit ' .. vim.fn.fnameescape(log_path))
+	vim.cmd 'setlocal readonly'
+	vim.cmd 'setlocal buftype=nowrite'
+end, { desc = 'Open nvim-dap log file' })
